416e5ca2a8cee3b650ade74c4862ad5240d00b9c
preveenraj <97preveenraj@gmail.com>
2021-05-01 21:47:06 +0530

    added west bengal

--- src/App.js
+++ src/App.js
@@ -16,20 +16,25 @@ const ageList = [
   {
     label: "👨 > 45",
     value: 50,
-  }
-]
+  },
+];
 function App() {
   const [centerInfo, setCenterInfo] = useState(null);
   const [activeDistrict, setActiveDistrict] = useState(null);
+  const [activeState, setActiveState] = useState("kerala");
   const [activeAgeCategory, setActiveAgeCategory] = useState(ageList[1]);
   const [isLoading, setIsLoading] = useState(false);
 
   const [activeDate, setActiveDate] = useState(getTomorrowsDate());
 
   const searchCenters = async () => {
-    if(activeDistrict) {
+    if (activeDistrict) {
       setIsLoading(true);
-      const data = await pingCowin({ districtId: activeDistrict, ageValue: activeAgeCategory?.value,  date: activeDate });
+      const data = await pingCowin({
+        districtId: activeDistrict?.value,
+        ageValue: activeAgeCategory?.value,
+        date: activeDate,
+      });
       setCenterInfo(data);
       setIsLoading(false);
     }
@@ -37,74 +42,110 @@ function App() {
 
   return (
     <div className="h-screen py-10 px-4 md:px-10 flex justify-center items-center bg-gray-700">
+      <div className="absolute top-2 flex gap-2">
+        <button
+          className={
+            activeState === "kerala"
+              ? "text-blue underline"
+              : "" + " cursor-pointer "
+          }
+          onClick={() => {
+            setActiveState("kerala");
+            setActiveDistrict(null);
+          }}
+        >
+          Kerala
+        </button>
+        <button
+          className={
+            activeState === "westbengal"
+              ? "text-blue underline"
+              : "" + " cursor-pointer "
+          }
+          onClick={() => {
+            setActiveState("westbengal");
+            setActiveDistrict(null);
+          }}
+        >
+          West Bengal
+        </button>
+      </div>
       <div className="h-full w-full flex flex-col gap-2 items-center md:w-1/2 p-2 rounded-lg border-2 border-gray-300 bg-gray-100 shadow-inner">
-       <div className="flex w-full gap-2">
-       <Select
-          isLoading={isLoading}
-          isClearable={true}
-          isSearchable={true}
-          placeholder={"Select District"}
-          error={false}
-          className="w-8/12"
-          name="districtList"
-          theme={(theme) => ({
-            ...theme,
-            borderRadius: 5,
-            colors: {
-              ...theme.colors,
-              primary: "#0097e6",
-            },
-          })}
-          options={districtConstants["kerala"]}
-          onChange={(selected) => setActiveDistrict(selected?.value || null)}
+        <div className="flex w-full gap-2">
+          <Select
+            isLoading={isLoading}
+            isClearable={true}
+            isSearchable={true}
+            placeholder={"Select District"}
+            error={false}
+            className="w-8/12"
+            name="districtList"
+            value={activeDistrict}
+            theme={(theme) => ({
+              ...theme,
+              borderRadius: 5,
+              colors: {
+                ...theme.colors,
+                primary: "#0097e6",
+              },
+            })}
+            options={districtConstants[activeState]}
+            onChange={(selected) => setActiveDistrict(selected || null)}
+          />
+          <Select
+            isLoading={false}
+            placeholder={"Select Age Category"}
+            error={false}
+            defaultValue={activeAgeCategory}
+            className="w-4/12 text-bold"
+            name="ageList"
+            theme={(theme) => ({
+              ...theme,
+              borderRadius: 5,
+              colors: {
+                ...theme.colors,
+                primary: "#0097e6",
+              },
+            })}
+            options={ageList}
+            onChange={(selected) => setActiveAgeCategory(selected || null)}
+          />
+        </div>
+        <DatePicker
+          dateFormat="MMMM d, yyyy"
+          className="w-full shadow-inner border-2 border-blue-300 p-2 rounded-lg text-center"
+          selected={activeDate}
+          onChange={(date) => setActiveDate(date)}
         />
-       <Select
-          isLoading={false}
-          placeholder={"Select Age Category"}
-          error={false}
-          defaultValue={activeAgeCategory}
-          className="w-4/12 text-bold"
-          name="ageList"
-          theme={(theme) => ({
-            ...theme,
-            borderRadius: 5,
-            colors: {
-              ...theme.colors,
-              primary: "#0097e6",
-            },
-          })}
-          options={ageList}
-          onChange={(selected) => setActiveAgeCategory(selected || null)}
-        />
-       </div>
-       <DatePicker 
-       dateFormat="MMMM d, yyyy"
-       className="w-full shadow-inner border-2 border-blue-300 p-2 rounded-lg text-center"
-       selected={activeDate} onChange={date => setActiveDate(date)} />
- 
+
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           onClick={searchCenters}
         >
           Search centers
         </button>
-       {centerInfo &&  <span className="text-xl rounded-lg border-2 text-gray-300 p-2">
-          Appoinments available: {centerInfo?.appointmentsAvailableCount || 0}
-        </span>}
-     {!!centerInfo?.centers.length &&    
-     <div className="p-8 flex flex-col gap-4 w-full overflow-scroll">
-          {centerInfo?.centers.map((center, index) => {
-            return (
-              <div key={index} className="w-full flex justify-between p-2 rounded-lg shadow-inner bg-white">
-                <span className="text-lg">{center.name}</span>
-                <span className="rounded-lg  p-1 h-8 font-bold text-green-400">
-                  {center.fee_type}
-                </span>
-              </div>
-            );
-          })}
-        </div>
-     }
+        {centerInfo && (
+          <span className="text-xl rounded-lg border-2 text-gray-300 p-2">
+            Appoinments available: {centerInfo?.appointmentsAvailableCount || 0}
+          </span>
+        )}
+        {!!centerInfo?.centers.length && (
+          <div className="p-8 flex flex-col gap-4 w-full overflow-scroll">
+            {centerInfo?.centers.map((center, index) => {
+              return (
+                <div
+                  key={index}
+                  className="w-full flex justify-between p-2 rounded-lg shadow-inner bg-white"
+                >
+                  <span className="text-lg">{center.name}</span>
+                  <span className="rounded-lg  p-1 h-8 font-bold text-green-400">
+                    {center.fee_type}
+                  </span>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     </div>
   );
--- src/constants/districts.js
+++ src/constants/districts.js
@@ -56,8 +56,127 @@ const kerala = [
     label: "Wayanad"
   },
 ];
-const districts = {
-  kerala
 
+const westbengal = [
+  {
+    "value": 710,
+    "label": "Alipurduar District"
+  },
+  {
+    "value": 711,
+    "label": "Bankura"
+  },
+  {
+    "value": 712,
+    "label": "Basirhat HD (North 24 Parganas)"
+  },
+  {
+    "value": 713,
+    "label": "Birbhum"
+  },
+  {
+    "value": 714,
+    "label": "Bishnupur HD (Bankura)"
+  },
+  {
+    "value": 715,
+    "label": "Cooch Behar"
+  },
+  {
+    "value": 783,
+    "label": "COOCHBEHAR"
+  },
+  {
+    "value": 716,
+    "label": "Dakshin Dinajpur"
+  },
+  {
+    "value": 717,
+    "label": "Darjeeling"
+  },
+  {
+    "value": 718,
+    "label": "Diamond Harbor HD (S 24 Parganas)"
+  },
+  {
+    "value": 719,
+    "label": "East Bardhaman"
+  },
+  {
+    "value": 720,
+    "label": "Hoogly"
+  },
+  {
+    "value": 721,
+    "label": "Howrah"
+  },
+  {
+    "value": 722,
+    "label": "Jalpaiguri"
+  },
+  {
+    "value": 723,
+    "label": "Jhargram"
+  },
+  {
+    "value": 724,
+    "label": "Kalimpong"
+  },
+  {
+    "value": 725,
+    "label": "Kolkata"
+  },
+  {
+    "value": 726,
+    "label": "Malda"
+  },
+  {
+    "value": 727,
+    "label": "Murshidabad"
+  },
+  {
+    "value": 728,
+    "label": "Nadia"
+  },
+  {
+    "value": 729,
+    "label": "Nandigram HD (East Medinipore)"
+  },
+  {
+    "value": 730,
+    "label": "North 24 Parganas"
+  },
+  {
+    "value": 731,
+    "label": "Paschim Medinipore"
+  },
+  {
+    "value": 732,
+    "label": "Purba Medinipore"
+  },
+  {
+    "value": 733,
+    "label": "Purulia"
+  },
+  {
+    "value": 734,
+    "label": "Rampurhat HD (Birbhum)"
+  },
+  {
+    "value": 735,
+    "label": "South 24 Parganas"
+  },
+  {
+    "value": 736,
+    "label": "Uttar Dinajpur"
+  },
+  {
+    "value": 737,
+    "label": "West Bardhaman"
+  }
+];
+const districts = {
+  kerala,
+  westbengal
 }
 export default districts;
