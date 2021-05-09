d9c80aa7da76b5ab7fc4a2eb11ca74ac9db25d5a
preveenraj <97preveenraj@gmail.com>
2021-05-09 18:43:21 +0530

    added telegram button

--- src/App.js
+++ src/App.js
@@ -168,6 +168,12 @@ const App = () => {
         >
           Search centers
         </button>
+          {(activeDistrict?.value === 304) && <span className="text-center text-xl rounded-lg border-2 text-gray-400 p-2">
+              Join 
+              <a className="text-blue-500 font-bold flex bg-blue-100 p-2 rounded-lg justify-center items-center h-12" href="https://t.me/joinchat/qcr00E6aZLxlZjI1">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-3.11-8.83l.013-.007.87 2.87c.112.311.266.367.453.341.188-.025.287-.126.41-.244l1.188-1.148 2.55 1.888c.466.257.801.124.917-.432l1.657-7.822c.183-.728-.137-1.02-.702-.788l-9.733 3.76c-.664.266-.66.638-.12.803l2.497.78z" fill="rgba(0,136,204,1)"/></svg>
+                Telegram Channel</a> to get notified instantly for vaccine availability in kottayam.
+            </span>}
         {centerInfo && (
           <>
             <span className="text-xl rounded-lg border-2 text-gray-400 p-2">
